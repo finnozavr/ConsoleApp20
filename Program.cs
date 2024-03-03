7e5a40f45a801f7f14f0d908e3cced254e6ceb9b
New <New@KOMPUTER>
2024-03-03 23:58:26 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double start = -3.14;
+            double end = 3.14;
+            double step = 0.2;
+
+            Console.WriteLine("|   x   |   f(x)   |");
+            Console.WriteLine("|-------|----------|");
+
+            for (double x = start; x <= end; x += step)
+            {
+                double result = Math.Sin(x);
+                Console.WriteLine($"| {x,5:F2}|{result,8:F4}|");
+            }
+            Console.ReadKey();
+        }
+    }
+}
